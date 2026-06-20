1dcae46d054d5fd3de4a5a6915c6e58b99c3607d
AhmedYeasin <ahmetchatgami@gmail.com>
2026-06-20 23:41:22 +0600

    GET API and middleweare successfully connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,7 @@
 import express from 'express';
+import { appRoutes } from './routes/appRoute.js';
 
 export const app = express();
+
+
+app.use('/', appRoutes);
---
+++ backend/src/routes/appRoute.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const appRoutes = Router(); 
+
+
+appRoutes.get("/", (req, res) =>{
+    res.send("app is running")
+})
