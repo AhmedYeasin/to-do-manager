550d6b0849cc98349ed355e7533bf106347d458d
AhmedYeasin <ahmetchatgami@gmail.com>
2026-06-24 22:20:29 +0600

    auth validation complete with Zod

--- backend/src/modules/auth/routes/authRoute.js
+++ backend/src/modules/auth/routes/authRoute.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { register } from "../controllers/authController.js";
+import { validate } from "../../../middlewares/validateMiddleware.js";
 
 export const authRoute = Router();
 
-authRoute.post('/register', register)
+authRoute.post('/register', validate(), register)
 
---
+++ backend/src/modules/auth/validations/authValidation.js
@@ -0,0 +1,17 @@
+import z, { regex } from "zod";
+import { validation } from "../../../shared/constants.js";
+
+const registerSchema = z.object({
+    body: z.object({
+        name: z.string().trim().min(1, 'Name is required').max(50, 'Name cannot exceed 50 characters'),
+
+        email: z.string().trim().toLowerCase().max(100, 'Email is too long.').pipe(z.email('Please provide a valid email')),
+
+        password: z.string()
+        .min(validation.passwordMinLength, `Password must be at least ${validation.passwordMinLength} characters long`)
+        .max(120, 'Password cannot exceed 120 characters')
+        .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+        .regex(/[0-9]/, 'Password must contain at least one number')
+        .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character')
+    })
+})
