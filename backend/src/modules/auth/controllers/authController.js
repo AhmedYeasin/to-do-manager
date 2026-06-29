d5ad18166e9dc552c7c2e2f4d3234e4a608d0c17
AhmedYeasin <ahmetchatgami@gmail.com>
2026-06-29 22:33:09 +0600

    API response for login is setup

--- backend/src/modules/auth/controllers/authController.js
+++ backend/src/modules/auth/controllers/authController.js
@@ -5,5 +5,10 @@ import { createAuthService } from "../services/authService.js";
 const authService = createAuthService();
 export const register = asyncHandler(async (req, res) => {
     const { user, accessToken, refreshToken } = await authService.register(req.body);
+    
     new ApiResponse(http_status.created, {user, accessToken, refreshToken}, 'user registered successfully').send(res);
+})
+
+export const login = asyncHandler(async(req, res)=>{
+    new ApiResponse(http_status.ok, {}, 'user logged in successfully').send(res);
 })
