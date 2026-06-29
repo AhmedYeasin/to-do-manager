a3643bb7f69b8d18ecd6a598c623443f12e47e6d
AhmedYeasin <ahmetchatgami@gmail.com>
2026-06-29 21:35:34 +0600

    API respone creating with JS Class

---
+++ backend/src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+export class ApiResponse {
+    constructor(statusCode, data, message = 'success') {
+        this.success = statusCode < 400
+        this.statusCode = statusCode
+        this.message = message
+        this.data = data
+    }
+}
