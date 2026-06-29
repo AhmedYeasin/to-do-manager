6f0250f7a3761054bf69bf438c5cd95942af563c
AhmedYeasin <ahmetchatgami@gmail.com>
2026-06-29 12:22:41 +0600

    password hashing completed with bcryptjs

--- backend/src/modules/auth/models/userModel.js
+++ backend/src/modules/auth/models/userModel.js
@@ -30,14 +30,14 @@ const userSchema = new mongoose.Schema({
         versionKey: false
     })
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next()
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return
     try {
         this.password = await bcrypt.hash(this.password, validation.bcrypt_salt_rounds)
-        next()
+
     }
     catch (error) {
-        next(error)
+        console.error(error)
     }
 })
 
