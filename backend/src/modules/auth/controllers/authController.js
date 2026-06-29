5a52fadf8fdb9a099857f535521294da1b701243
AhmedYeasin <ahmetchatgami@gmail.com>
2026-06-29 22:06:42 +0600

    API Response, json data converted to object

--- backend/src/modules/auth/controllers/authController.js
+++ backend/src/modules/auth/controllers/authController.js
@@ -1,13 +1,9 @@
+import { ApiResponse } from "../../../utils/apiResponse.js";
 import { asyncHandler } from "../../../utils/asyncHandler.js";
 import { createAuthService } from "../services/authService.js";
 
 const authService = createAuthService();
 export const register = asyncHandler(async (req, res) => {
     const { user, accessToken, refreshToken } = await authService.register(req.body);
-    res.status(http_status.created).json({
-        user,
-        accessToken,
-        refreshToken
-    }, 'user registered successfully'
-    )
+    new ApiResponse(http_status.created, {user, accessToken, refreshToken}, 'user registered successfully').send(res);
 })
