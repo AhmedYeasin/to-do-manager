f75be9208134c73ad85b62670b4b345e0dcffd38
AhmedYeasin <ahmetchatgami@gmail.com>
2026-06-29 22:28:08 +0600

    Login Schema and route setup

--- backend/src/modules/auth/routes/authRoute.js
+++ backend/src/modules/auth/routes/authRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { register } from "../controllers/authController.js";
 import { validate } from "../../../middlewares/validateMiddleware.js";
+import { loginSchema, registerSchema } from "../validations/authValidation.js";
 
 export const authRoute = Router();
 
-authRoute.post('/register', validate(), register)
-
+authRoute.post('/register', validate(registerSchema), register)
+authRoute.post('/login', validate(loginSchema), login)
--- backend/src/modules/auth/validations/authValidation.js
+++ backend/src/modules/auth/validations/authValidation.js
@@ -1,18 +1,30 @@
 import z, { regex } from "zod";
 import { validation } from "../../../shared/constants.js";
 
-const registerSchema = z.object({
+
+const emailField = z.string().trim().toLowerCase().max(100, 'Email is too long.').pipe(z.email('Please provide a valid email')),
+
+const passwordField = z.string()
+    .min(validation.passwordMinLength, `Password must be at least ${validation.passwordMinLength} characters long`)
+    .max(120, 'Password cannot exceed 120 characters')
+    // .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[0-9]/, 'Password must contain at least one number')
+    .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character')
+
+export const registerSchema = z.object({
     body: z.object({
         name: z.string().trim().min(1, 'Name is required').max(50, 'Name cannot exceed 50 characters'),
 
-        email: z.string().trim().toLowerCase().max(100, 'Email is too long.').pipe(z.email('Please provide a valid email')),
+        email: emailField,
+        password: passwordField
 
-        password: z.string()
-        .min(validation.passwordMinLength, `Password must be at least ${validation.passwordMinLength} characters long`)
-        .max(120, 'Password cannot exceed 120 characters')
-        // .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character')
-        .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-        .regex(/[0-9]/, 'Password must contain at least one number')
-        .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character')
+    })
+})
+export const loginSchema = z.object({
+    body: z.object({
+        email: emailField,
+        password: z.string().min(1, 'Password is required').max(120, 'Password cannot exceed 120 characters')
+
     })
 })
