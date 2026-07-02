4a9cc4df5c7c233cfc3585ae5a7a0483e5222741
AhmedYeasin <ahmetchatgami@gmail.com>
2026-07-02 21:58:32 +0600

    now user authenticating with JS Class

--- backend/src/modules/auth/repositories/authRepositoy.js
+++ backend/src/modules/auth/repositories/authRepositoy.js
@@ -1,19 +1,29 @@
-import { User } from "../models/userModel.js";
+// import { User } from "../models/userModel.js";
 
-export const createAuthRepository = () => {
-    return {
-        create: async (userData) => {
-            const user = await User.create(userData);
-            const { password, ...userWithoutPassword } = user.toObject();
-            return userWithoutPassword;
-        },
+// export const createAuthRepository = () => {
+//     return {
+//         create: async (userData) => {
+//             const user = await User.create(userData);
+//             const { password, ...userWithoutPassword } = user.toObject();
+//             return userWithoutPassword;
+//         },
 
-        findById: async (id) => {
-            return User.findById(id)
-        },
+//         findById: async (id) => {
+//             return User.findById(id)
+//         },
 
-        findByEmail: async (email) => {
-            return User.findOne({ email }).select("+password").lean()
-        }
+//         findByEmail: async (email) => {
+//             return User.findOne({ email }).select("+password").lean()
+//         }
+//     }
+// }
+
+export class UserRepository{
+    constructor(model = User){
+        this.model = model
+    }
+
+    async findByEmail(email){
+        return this.model.findOne({email}).select("+password")
     }
 }
--- backend/src/modules/auth/services/authService.js
+++ backend/src/modules/auth/services/authService.js
@@ -1,43 +1,63 @@
-import { generateAccessToken, generateRefreshToken } from "../../../utils/jwt.js";
-import { createAuthRepository } from "../repositories/authRepositoy.js";
+// import { generateAccessToken, generateRefreshToken } from "../../../utils/jwt.js";
+// import { createAuthRepository } from "../repositories/authRepositoy.js";
 
-export const createAuthService = (userRepository = createAuthRepository()) => {
+import { UserRepository } from "../repositories/authRepositoy.js";
 
-    const generateTokenPair = async (userId) => {
-        const accessToken = await generateAccessToken(userId)
-        const refreshToken = await generateRefreshToken(userId)
-        return {
-            accessToken,
-            refreshToken
-        }
+// export const createAuthService = (userRepository = createAuthRepository()) => {
+
+//     const generateTokenPair = async (userId) => {
+//         const accessToken = await generateAccessToken(userId)
+//         const refreshToken = await generateRefreshToken(userId)
+//         return {
+//             accessToken,
+//             refreshToken
+//         }
+//     }
+
+//     return {
+//         register: async ({ name, email, password }) => {
+//             const user = await userRepository.create({ name, email, password });
+//             const tokens = await generateTokenPair(user._id);
+//             return {
+//                 user,
+//                 ...tokens
+//             };
+
+//         },
+
+//         login: async ({ email, password }) => {
+//             const user = await userRepository.findByEmail(email)
+
+//             const isMatch = await bcrypt.compare(password, user.password)
+//             if (!isMatch) {
+//                 console.error("Invalid email or password")
+//             }
+//             const { password: _, ...userWithoutPassword } = user
+
+//             const tokens = await generateTokenPair(user._id);
+//             return {
+//                 user: userWithoutPassword,
+//                 ...tokens
+//             }
+//         }
+//     }
+
+// }
+
+export class AuthService {
+    #userRepository
+    constructor(userRepo = new UserRepository()) {
+        this.#userRepository = userRepo;
     }
 
-    return {
-        register: async ({ name, email, password }) => {
-            const user = await userRepository.create({ name, email, password });
-            const tokens = await generateTokenPair(user._id);
-            return {
-                user,
-                ...tokens
-            };
+    async register({ name, email, password }) {
+        const existingUser = await this.#userRepository.findByEmail(email)
 
-        },
+        if (existingUser) {
+            console.error(message = "User already exists")
+        }
+        else {
 
-        login: async ({ email, password }) => {
-            const user = await userRepository.findByEmail(email)
-
-            const isMatch = await bcrypt.compare(password, user.password)
-            if (!isMatch) {
-                console.error("Invalid email or password")
-            }
-            const { password: _, ...userWithoutPassword } = user
-
-            const tokens = await generateTokenPair(user._id);
-            return {
-                user: userWithoutPassword,
-                ...tokens
-            }
         }
     }
-
 }
