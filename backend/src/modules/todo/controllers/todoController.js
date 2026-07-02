afefdc8d3b096052d0b1a7db1079351fd982ae78
AhmedYeasin <ahmetchatgami@gmail.com>
2026-07-02 21:31:34 +0600

    error handled in object

--- backend/src/modules/todo/controllers/todoController.js
+++ backend/src/modules/todo/controllers/todoController.js
@@ -1,8 +1,10 @@
 import { http_status } from "../../../shared/constants.js";
 import { ApiResponse } from "../../../utils/apiResponse.js";
 import { asyncHandler } from "../../../utils/asyncHandler.js";
-import { todoService } from "../services/todoService.js";
+import { createTodoService, todoService } from "../services/todoService.js";
 
+
+const todoService = createTodoService()
 export const createTodo = asyncHandler(async (req, res)=>{
   const todo = await todoService.create(req.body)
   new ApiResponse(http_status.created, todo, 'Todo created successfully').send(res)
--- backend/src/modules/todo/services/todoService.js
+++ backend/src/modules/todo/services/todoService.js
@@ -1,11 +1,11 @@
 import { createTodoRepository } from "../respositories/todoRepository.js"
 
-export const todoService = () => {
+export const createTodoService = (todoRepository = createTodoRepository()) => {
     return {
         // Logic to create a new todo item
         create: async (todoData) => {
             try {
-                return await createTodoRepository.create(todoData)
+                return await todoRepository.create(todoData)
             } catch (error) {
                 console.error("Error creating todo")
             }
